610143e3214184e264971afb8ea2ede5caaf51df
aminerve <122647915+aminerve@users.noreply.github.com>
2023-03-29 13:20:41 -0400

    day3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,13 @@ import React from 'react'
 import About from './About'
 import Footer from './Footer'
 import Projects from './Projects'
+import img1 from '../images/AIDEN.JPG'
 
 export default function Home() {
   return (
     <div>
     <h1>Welcome to My Page...</h1>
-    <img src='' alt='Aiden Minerve'/>
+    <img src={img1} alt='Aiden Minerve'/>
     <About/>
     <Projects/>
     <Footer/>
