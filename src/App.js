02f455c3bf10e990d271eed764b85dc6cdc1bbe9
aminerve <122647915+aminerve@users.noreply.github.com>
2023-03-30 22:16:04 -0400

    day5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
 import { Route, Routes } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
 
 import './App.css';
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,9 @@
 import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap'
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import social1 from '../images/nav-icon1.svg'
+import social2 from '../images/nav-icon2.svg'
+import social3 from '../images/nav-icon3.svg'
 
 export default function NavBar() {
   const [activeLink, setActiveLink] = useState('home')
@@ -21,7 +24,9 @@ export default function NavBar() {
     return () => window.removeEventListener("scroll", onScroll)
   },[])
 
-
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value)
+  }
 
   return (                      //WHAT      TRUE     FALSE
 <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
@@ -32,16 +37,16 @@ export default function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-            <Nav.Link href="#about"className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'}onClick={() => onUpdateActiveLink('about')}>About Me</Nav.Link>
-            <Nav.Link href="#projects"className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-            <Nav.Link href="#contact"className={activeLink === 'contact' ? 'active navbar-link' : 'navbar-link'}onClick={() => onUpdateActiveLink('contact')}>Contact Me</Nav.Link>
+            {/* <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>HOME</Nav.Link> */}
+            <Nav.Link href="#about"className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'}onClick={() => onUpdateActiveLink('about')}>ABOUT ME</Nav.Link>
+            <Nav.Link href="#projects"className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}onClick={() => onUpdateActiveLink('projects')}>PROJECTS</Nav.Link>
+            <Nav.Link href="#contact"className={activeLink === 'contact' ? 'active navbar-link' : 'navbar-link'}onClick={() => onUpdateActiveLink('contact')}>CONTACT ME</Nav.Link>
           </Nav>
           <span className='navbar-socials'>
             <div className='social-icons'>
-              <a href='#'><img src={''} alt='linkedin'/></a>
-              <a href='#'><img src={''} alt='github'/></a>
-              <a href='#'><img src={''} alt='facebook'/></a>
+              <a href='https://www.linkedin.com/in/aiden-minerve-567085266/'><img src={social1} alt='linkedin'/></a>
+              <a href='https://github.com/aminerve'><img src={social2} alt='github'/></a>
+              <a href='#'><img src={social3} alt='facebook'/></a>
             </div>
           </span>
         </Navbar.Collapse>
